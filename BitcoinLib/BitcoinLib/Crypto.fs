6083ab8b1ddee12b3613c436c51da8fac2f70bb7
Tim <TimCodesStuff@hotmail.com>
2019-06-16 03:54:52 -0700

    Beginning to port BitcoinLibCSharp to F#
    Adding a few functions for crypto and for encoding. More to come.

---
+++ BitcoinLib/BitcoinLib/Crypto.fs
@@ -0,0 +1,30 @@
+﻿module Crypto
+
+open System.Security.Cryptography
+open System
+
+let Sha256 (array : byte[]) =
+    (new SHA256Managed()).ComputeHash(array)
+
+let RipeMD160 (array : byte[]) =
+    (new RIPEMD160Managed()).ComputeHash(array)
+
+let GenerateRand256BitKey () =
+    let key : byte[] = Array.zeroCreate 32
+    (RandomNumberGenerator.Create()).GetBytes(key)
+    key
+
+let GenerateRandECDSACompliant256BitKey () =
+    let upperHexLimit = "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364140"
+    let rng = RandomNumberGenerator.Create()
+    let key : byte[] = Array.zeroCreate 32
+
+    let rec getKeyInBounds() =
+        rng.GetBytes(key)
+        let hexKey = (Encoding.ByteArrayToHexString false key).ToLower()
+        if (String.Compare(hexKey, upperHexLimit, true) >= 0) then
+            getKeyInBounds()
+        else
+            key
+
+    getKeyInBounds()
---
+++ BitcoinLib/BitcoinLib/Encoding.fs
@@ -0,0 +1,8 @@
+﻿module Encoding
+
+open System
+
+let ByteArrayToHexString (includeDashes : bool) (byteArray : byte[]) =
+    match includeDashes with
+    | true -> BitConverter.ToString(byteArray).ToLower()
+    | false -> BitConverter.ToString(byteArray).Replace("-", "").ToLower()
