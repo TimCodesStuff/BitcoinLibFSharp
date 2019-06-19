8088f538ee959dfd50d0ccb75f6240fe0941ef72
Tim <TimCodesStuff@hotmail.com>
2019-06-19 02:36:22 -0700

    Adding ByteArrayToBigInt function.
    Added the ByteArrayToBigInt funtion. I'll need this later for Base58Encode.
    I also added a few more tests. These tests only cover a known good input, but I will add more later to test bad input.

--- BitcoinLib/BitcoinLib/Encoding.fs
+++ BitcoinLib/BitcoinLib/Encoding.fs
@@ -2,6 +2,7 @@
 
 open System
 open System.Linq
+open System.Numerics
 
 let HexStringToByteArray (hex : string) = 
     Enumerable.Range(0, hex.Length)
@@ -13,3 +14,14 @@ let ByteArrayToHexString (includeDashes : bool) (byteArray : byte[]) =
     match includeDashes with
     | true -> BitConverter.ToString(byteArray).ToLower()
     | false -> BitConverter.ToString(byteArray).Replace("-", "").ToLower()
+
+let ByteArrayToBigInt (array : byte[]) =
+    let rec buildBigInt (bigInt : BigInteger) (arr : byte[]) =
+        if arr.Length = 0 then
+            bigInt
+        else
+            let bi = (bigInt * (bigint 256)) + (BigInteger(int(arr.[0])))
+            let arrayTail = arr.[1..]
+            buildBigInt bi arrayTail
+
+    buildBigInt (bigint 0) array
--- BitcoinLib/BitcoinLibUnitTests/EncodingTests.fs
+++ BitcoinLib/BitcoinLibUnitTests/EncodingTests.fs
@@ -7,13 +7,41 @@ open System
 
 [<TestClass>]
 type EncodingTests () =
+    let comparisonFunc = Array.compareWith(fun x y -> if x = y then 0 else 1)
 
     [<TestMethod>]
     member this.TestHexStringToByteArray () =
-        let comparisonFunc = Array.compareWith(fun x y -> if x = y then 1 else 0)
-
         let expected = [| byte(24); byte(225); byte(74); byte(123); byte(106); byte(48); byte(127); byte(66); byte(106); byte(148); byte(248); byte(17); byte(71); byte(1); byte(231); byte(200); byte(231); byte(116); byte(231); byte(249); byte(164); byte(126); byte(44); byte(32); byte(53); byte(219); byte(41); byte(162); byte(6); byte(50); byte(23); byte(37); |]
         let actual = Encoding.HexStringToByteArray "18e14a7b6a307f426a94f8114701e7c8e774e7f9a47e2c2035db29a206321725"
         let result = comparisonFunc expected actual
-        Assert.AreEqual(result, 1)
-        
+        Assert.AreEqual(result, 0)
+    
+    [<TestMethod>]
+    member this.TestByteArrayToHexString () =
+        let expected = "18e14a7b6a307f426a94f8114701e7c8e774e7f9a47e2c2035db29a206321725"
+        let byteArray = [| byte(24); byte(225); byte(74); byte(123); byte(106); byte(48); byte(127); byte(66); byte(106); byte(148); byte(248); byte(17); byte(71); byte(1); byte(231); byte(200); byte(231); byte(116); byte(231); byte(249); byte(164); byte(126); byte(44); byte(32); byte(53); byte(219); byte(41); byte(162); byte(6); byte(50); byte(23); byte(37); |]
+        let actual = Encoding.ByteArrayToHexString false byteArray
+        Assert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestByteArrayToHexStringWithDashes () =
+        let expected = "18-e1-4a-7b-6a-30-7f-42-6a-94-f8-11-47-01-e7-c8-e7-74-e7-f9-a4-7e-2c-20-35-db-29-a2-06-32-17-25"
+        let byteArray = [| byte(24); byte(225); byte(74); byte(123); byte(106); byte(48); byte(127); byte(66); byte(106); byte(148); byte(248); byte(17); byte(71); byte(1); byte(231); byte(200); byte(231); byte(116); byte(231); byte(249); byte(164); byte(126); byte(44); byte(32); byte(53); byte(219); byte(41); byte(162); byte(6); byte(50); byte(23); byte(37); |]
+        let actual = Encoding.ByteArrayToHexString true byteArray
+        Assert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestHexToByteToHex () =
+        let expected = "18e14a7b6a307f426a94f8114701e7c8e774e7f9a47e2c2035db29a206321725"
+        let actual = 
+            expected
+            |> Encoding.HexStringToByteArray
+            |> Encoding.ByteArrayToHexString false
+        Assert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.TestByteArrayToBigInt () =
+        let expected = bigint 417417851
+        let actual = Encoding.ByteArrayToBigInt [| byte(24); byte(225); byte(74); byte(123); |]
+        Assert.AreEqual(expected, actual)
+
