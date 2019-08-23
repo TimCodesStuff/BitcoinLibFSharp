5acb4d9e636961ff5049cb982c20d417cc6f47b5
Tim <TimCodesStuff@hotmail.com>
2019-08-23 14:01:09 -0700

    Adding unit tests for the crypto functions.
    Adding tests to ensure that exceptions in the underlying crypto functions are handled correctly. I'd rather the consumer be forced to deal with them at compile-time than have a run-time exception thrown.

---
+++ BitcoinLib/BitcoinLibUnitTests/CryptoTests.fs
@@ -0,0 +1,21 @@
+﻿module CryptoTests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type CryptoTests () =
+    
+    [<TestMethod>]
+    member this.TestBadInputRipeMd () =
+        Crypto.RipeMD160(null)
+        |> function
+        | Result.Ok _ -> Assert.Fail "Expected error when calling Crypto.RipeMD160(null)."
+        | Result.Error _ -> ()
+        
+
+    [<TestMethod>]
+    member this.TestGoodInputRipeMd () =
+        Crypto.RipeMD160([|byte(171); byte(205)|])
+        |> function
+        | Result.Error m -> Assert.Fail m
+        | Result.Ok _ -> () // No need to test that the underlying value is correct, assume the library works correctly. 
